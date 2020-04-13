2969ac5eaad2ec698c43a64f24dd41829a5cf22a
Joshua Peters <peters.l.joshua@gmail.com>
2020-04-13 15:00:59 -0500

    creating person component, first component in project structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
+        // This is the HTML code or JSX - Syntactical sugar for React
       <div className="App">
         <h1>Hi, I'm a react app.</h1>
+        <Person/>
       </div>
     );
   }
